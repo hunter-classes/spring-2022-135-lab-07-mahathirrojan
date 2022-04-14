18a5e65d3c4b6a391dd5072267eedc72f7f1e867
Aurnab Das <aurnabdas@Aurnabs-MacBook-Pro.local>
2022-04-14 10:10:39 -0400

    added change to indent.cpp - aurnab das

--- indent.cpp
+++ indent.cpp
@@ -14,3 +14,16 @@ std::string indent(std::string line, int out, int in) {
     return result + line; 
 }
 
+int countChar(std::string line, char c)
+{
+    int count = 0;
+    for (int i = 0; i < line.length(); i++)
+    {
+        if (line[i] == c)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
