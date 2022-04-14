fa5907f429887efc1feae55ad630e98ccd9d824a
Mahathir Rojan <torbengalifreind@gmail.com>
2022-04-14 09:34:31 -0400

    changed func - Mahathir Rojan

--- indent.cpp
+++ indent.cpp
@@ -1,21 +1,19 @@
 #include "indent.h"
 #include <string>
 
-int countChar(std::string line, char c) { 
-    int total = 0; 
-
-    for(int i = 0; i < line.length(); i++) { 
-        if(line[i] == c) { 
-            total++; 
+int countChar(std::string line, char c)
+{
+    int count = 0;
+    for (int i = 0; i < line.length(); i++)
+    {
+        if (line[i] == c)
+        {
+            count++;
         }
     }
-    
-    if(line.find("{") < line.find("}") && line.find("{") != -1 && line.find("}") != -1) { 
-        return total - 1;
-    } else { 
-        return total;
-    }
+    return count;
 }
+
 
 std::string indent(std::string line, int out, int in) { 
     std::string result = ""; 
