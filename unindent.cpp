971e69d64d719a31d438a13691af06ba1a3f8051
Mahathir Rojan <torbengalifreind@gmail.com>
2022-04-20 20:30:16 -0400

    change made for second partner - Mahathir Rojan

--- unindent.cpp
+++ unindent.cpp
@@ -5,18 +5,3 @@
 #include "unindent.h"
 
 
-std::string removeLeadingSpaces(std::string line)
-{
-  int len = line.length();
-  std::string output;
-
-  int i = 0;
-  if (isspace(line[i])){
-      do{
-	  i++;
-      }while (isspace(line[i]));
-  }
-
-  output = line.substr(i);
-  return output;
-}
